b7183daab097b84ec2ef8e19648b33c3c5059541
Alojz Holúbek <93535633+likoli007@users.noreply.github.com>
2024-04-01 10:49:25 +0800

    Start of Qt Widget Application developpment

--- Knittee.cpp
+++ Knittee.cpp
@@ -4,18 +4,11 @@ Knittee::Knittee(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
-    
-    QWidget* topMenuWidget = new QWidget;
-    setCentralWiget(topMenuWidget);
+   
 
 
 }
 
-QMenuBar topMenu(QWidget* parent) 
-{
-
-
-}
 
 Knittee::~Knittee()
 {}
